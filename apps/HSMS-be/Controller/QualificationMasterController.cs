9086841578e986ec6e96ac3ded1f56d672acd7d1
NareshKumar <naresh.kumar@ifundcities.com>
2026-03-23 23:32:56 +0530

    add Qualification

---
+++ apps/HSMS-be/Controller/QualificationMasterController.cs
@@ -0,0 +1,52 @@
+﻿using HSMS.Application.IServices;
+using HSMS.contracts.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HSMS_be.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QualificationMasterController : ControllerBase
+    {
+        private readonly QualificationMastersIService _Service;
+
+        public QualificationMasterController(QualificationMastersIService Service)
+        {
+            _Service = Service;
+        }
+
+        [HttpPost("CreateQualificationMaster")]
+        public async Task<IActionResult> CreateQualificationMaster(Qualification dto)
+        {
+            var res = await _Service.CreateQualificationMasterAsync(dto);
+            if (!res.IsSuccess)
+            {
+                return BadRequest(res.ErrorMessage);
+            }
+            return Ok(res.Data);
+        }
+
+        [HttpDelete("DeleteQualificationMaster/{id}")]
+        public async Task<IActionResult> DeleteQualificationMaster(int id)
+        {
+            var res = await _Service.DeleteQualificationMasterAsync(id);
+            if (!res.IsSuccess)
+            {
+                return BadRequest(res.ErrorMessage);
+            }
+            return Ok(res.Data);
+        }
+
+        [HttpGet("GetALLQualificationMaster")]
+        public async Task<IActionResult> GetQualificationMaster()
+        {
+            var res = await _Service.GetQualificationMasterAsync();
+            if (!res.IsSuccess)
+            {
+                return BadRequest(res.ErrorMessage);
+            }
+            return Ok(res.Data);
+        }
+    }
+}
